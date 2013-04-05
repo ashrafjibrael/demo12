0a3545922336862dcde4fe6499be8051bee57d70
Ashraf Jibrael <ashrafjanan@Ashrafs-MacBook-Pro.local>
2013-04-05 11:08:45 +0200

    Added project

---
+++ Demofile.h
@@ -0,0 +1,13 @@
+//
+//  Demofile.h
+//  demo12
+//
+//  Created by Ashraf Jibrael on 4/5/13.
+//  Copyright (c) 2013 Ashraf Jibrael. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Demofile : NSObject
+
+@end
---
+++ Demofile.m
@@ -0,0 +1,13 @@
+//
+//  Demofile.m
+//  demo12
+//
+//  Created by Ashraf Jibrael on 4/5/13.
+//  Copyright (c) 2013 Ashraf Jibrael. All rights reserved.
+//
+
+#import "Demofile.h"
+
+@implementation Demofile
+
+@end
