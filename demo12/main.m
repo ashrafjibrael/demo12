9633d62aedc6a5076c97426786b6eadcd15cf7b8
Ashraf Jibrael <ashrafjanan@Ashrafs-MacBook-Pro.local>
2013-04-05 11:06:38 +0200

    Initial Commit

---
+++ demo12/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  demo12
+//
+//  Created by Ashraf Jibrael on 4/5/13.
+//  Copyright (c) 2013 Ashraf Jibrael. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
